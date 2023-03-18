48782395cc8743e3be85e57c7d16d8f6794ba1f4
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-03-18 15:16:47 -0400

    Update compile.sh
    better bash

--- compile.sh
+++ compile.sh
@@ -1,1 +1,11 @@
-g++ main.cpp fuelorder.cpp socket-server.cpp -o program; ./program
+#!/bin/bash
+
+# This script compiles and runs the program.
+
+set -euo pipefail
+
+# Compile the program with all warnings enabled, C++11 features enabled and pthread library linked
+g++ -Wall -Werror -std=c++11 -pthread main.cpp fuelorder.cpp socket-server.cpp -o program
+
+# Run the program
+./program
